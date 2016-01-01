7d868ccfb62cda3c58817120acfd0be14cd4818e
Alex Bennett <alexandergraybennett@gmail.com>
2016-01-01 14:05:35 -0700

    makin tables

--- migrations/20160101140119_bucket_list.js
+++ migrations/20160101140119_bucket_list.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('bucket', function(table){
+    table.increments();
+    table.string('name');
+    table.integer('awesomeness');
+    table.boolean('completed');
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('bucket');
 };
