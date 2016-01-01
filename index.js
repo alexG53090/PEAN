b61d568c4ea7952053322f123256bc8dd065d71b
Alex Bennett <alexandergraybennett@gmail.com>
2016-01-01 13:01:19 -0700

    configs

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var cors = require('cors');
+
+app.use(cors, cors())
+
 
 app.get('/', function(req, res, next){
   res.send('sup, Foo? Nothing, Bar!')
