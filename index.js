0e2e7578aecf6b7c0ba42a7a421f672a2cb755e9
Alex Bennett <alexandergraybennett@gmail.com>
2016-01-01 13:03:57 -0700

    use modules

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+// Requirements
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var cors = require('cors');
 
-app.use(cors, cors())
-
+// use modules
+app.use(express.static('./views'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 app.get('/', function(req, res, next){
   res.send('sup, Foo? Nothing, Bar!')
