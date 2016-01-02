d348d915650d3125ccb7a9faa2d1e2e8b9ee73e8
Alex Bennett <alexandergraybennett@gmail.com>
2016-01-02 11:35:33 -0700

    Create db entry :white_check_mark:

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 // Requirements
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var cors = require('cors');
-
+var knex = require('./db/knex');
+var app = express();
 // use modules
 app.use(express.static('./views'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-app.get('/', function(req, res, next){
-  res.redirect('/index.html')
+// get some routes goin'
+var router = express();
+
+// use dem routes
+// app.use('/enter', router)
+
+app.post('/addwish', function(req, res, next){
+  knex('lastwish').insert({
+    user: req.body.user,
+    wish: req.body.wish,
+    radness: req.body.radness,
+    complete: req.body.complete,
+  }, 'user').then(function(user){
+    console.log('fuck');
+  })
+})
+
+app.get('/main', function(req, res, next){
+  res.redirect('/main.html')
 })
 
 app.listen(3000, function(){
