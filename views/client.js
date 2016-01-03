9c5a7b25706b933a72e059f92c42ab2d2db862cb
Alex Bennett <alexandergraybennett@gmail.com>
2016-01-03 10:18:25 -0700

    chart data :bar_chart:

--- views/client.js
+++ views/client.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
   getWishData().then(function(data){
     formatWishData(data).then(function(wishes){
-      formatWishes(wishes).then(function(wishes){
-        console.log(wishes)
+      formatWishes(wishes).then(function(wishArray){
+        dataVisual(wishArray).then(function(wishArray){
+          console.log(wishArray);
+        })
       })
     })
   })
@@ -28,11 +30,12 @@ function formatWishData(data){
 }
 
 function formatWishes(wishes){
-  var activities = [];
-  var radnessLevels = [];
-  var completedWishes = [];
-  var people = [];
   return new Promise(function(resolve, reject){
+    var activities = [];
+    var radnessLevels = [];
+    var completedWishes = [];
+    var people = [];
+    var wishArray = [];
     wishes.forEach(function(item, index, array){
       var activity = item.wish;
       var radLevel = item.radness;
@@ -43,10 +46,30 @@ function formatWishes(wishes){
       completedWishes.push(status);
       people.push(person);
     })
-    wishes.push(activities);
-    wishes.push(radnessLevels);
-    wishes.push(completedWishes);
-    wishes.push(people);
-    resolve(wishes);
+    activities.unshift('wishes');
+    wishArray.push(activities);
+    wishArray.push(radnessLevels);
+    wishArray.push(completedWishes);
+    wishArray.push(people);
+    resolve(wishArray);
   });
 };
+
+function dataVisual(wishArray){
+  return new Promise(function(resolve, reject){
+    var data = wishArray;
+
+    var x = d3.scale.linear()
+      .domain([0, d3.max(data)])
+      .range([0, 420]);
+
+    d3.select(".chart")
+      .selectAll("div")
+      .data(data)
+
+    .enter().append("div")
+      .style("width", function(d) { return x(d) + "px"; })
+      .text(function(d) { return d; });
+      console.log(wishArray)
+  })
+}
