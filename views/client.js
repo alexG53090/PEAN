fa38fc0435b3d3b4c9143abb724c79a221a43312
Alex Bennett <alexandergraybennett@gmail.com>
2016-01-03 10:02:05 -0700

    format data :triangular_ruler:

--- views/client.js
+++ views/client.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
   getWishData().then(function(data){
-    console.log(data);
+    formatWishData(data).then(function(wishes){
+      formatWishes(wishes).then(function(wishes){
+        console.log(wishes)
+      })
+    })
   })
 })
 
@@ -9,5 +13,40 @@ function getWishData(){
     $.get('/get', function(data){
       resolve(data);
     });
+  });
+};
+
+function formatWishData(data){
+  return new Promise(function(resolve, reject){
+    var wishes = [];
+    for(var i = 0; i < data.lastwish.length; i = i + 1){
+      var oneWish = data.lastwish[i];
+      wishes.push(oneWish);
+    }
+    resolve(wishes);
+  })
+}
+
+function formatWishes(wishes){
+  var activities = [];
+  var radnessLevels = [];
+  var completedWishes = [];
+  var people = [];
+  return new Promise(function(resolve, reject){
+    wishes.forEach(function(item, index, array){
+      var activity = item.wish;
+      var radLevel = item.radness;
+      var status = item.complete;
+      var person = item.user;
+      activities.push(activity);
+      radnessLevels.push(radLevel);
+      completedWishes.push(status);
+      people.push(person);
+    })
+    wishes.push(activities);
+    wishes.push(radnessLevels);
+    wishes.push(completedWishes);
+    wishes.push(people);
+    resolve(wishes);
   });
 };
